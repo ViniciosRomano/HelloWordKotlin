cb7775a34e22e34c34235b872be5489e5adb1f2c
logonrmlocal <rm93018@fiap.com.br>
2024-02-28 20:56:09 -0300

    Up

--- app/src/main/java/viniciosromano/com/github/helloword/MainActivity.kt
+++ app/src/main/java/viniciosromano/com/github/helloword/MainActivity.kt
@@ -1,17 +1,19 @@
 package viniciosromano.com.github.helloword
 
 import android.os.Bundle
-import android.widget.TextView
+//import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity (){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //Criando um objeto de texto
-        val texto = TextView(this)
-        texto.text = "hello Kotlin"
+//        //Criando um objeto de texto
+//        val texto = TextView(this)
+//        texto.text = "hello Kotlin"
 
-        setContentView(texto)
+        //setContentView(texto)
+        setContentView(R.layout.activity_main)
+
     }
 }
